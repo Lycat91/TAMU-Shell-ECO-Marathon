c494443d55e8227d35db60ea43e67e0ab9bf7793
Lycat91 <158769561+Lycat91@users.noreply.github.com>
2025-11-11 09:52:30 -0600

    comment

--- DIS/device/comm.py
+++ DIS/device/comm.py
@@ -2,66 +2,13 @@ from machine import Pin, SPI
 from machine import UART, Pin
 import time
 import math
-import framebuf
+
 
 DC = 8
 RST = 12
 MOSI = 11
 SCK = 10
 CS = 9
-
-KEY0 = Pin(15, Pin.IN, Pin.PULL_UP)  # Active LOW
-KEY1 = Pin(17, Pin.IN, Pin.PULL_UP)  # Active LOW
-
-class OLED_1inch3(framebuf.FrameBuffer):
-    def __init__(self):
-        self.width = 128
-        self.height = 64
-        self.rotate = 180
-
-        self.cs = Pin(CS, Pin.OUT)
-        self.rst = Pin(RST, Pin.OUT)
-        self.dc = Pin(DC, Pin.OUT)
-        self.cs(1)
-
-        self.spi = SPI(1, baudrate=20000000, polarity=0, phase=0,
-                       sck=Pin(SCK), mosi=Pin(MOSI))
-
-        self.buffer = bytearray(self.width * self.height // 8)
-        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_HMSB)
-        self.init_display()
-
-    def write_cmd(self, cmd):
-        self.cs(1); self.dc(0); self.cs(0)
-        self.spi.write(bytearray([cmd]))
-        self.cs(1)
-
-    def write_data(self, buf):
-        self.cs(1); self.dc(1); self.cs(0)
-        self.spi.write(bytearray([buf]))
-        self.cs(1)
-
-    def init_display(self):
-        self.rst(1); time.sleep(0.001)
-        self.rst(0); time.sleep(0.01)
-        self.rst(1)
-        for cmd in [
-            0xAE, 0x00, 0x10, 0xB0, 0xDC, 0x00,
-            0x81, 0x6F,
-            0xAF, 0x21, 0xA1, 0xC0, 0xA4, 0xA6, 0xA8, 0x3F,
-            0xD3, 0x60, 0xD5, 0x41, 0xD9, 0x22, 0xDB, 0x35,
-            0xAD, 0x8A, 0xAF
-        ]:
-            self.write_cmd(cmd)
-
-    def show(self):
-        self.write_cmd(0xB0)
-        for page in range(0, 64):
-            column = page if self.rotate == 180 else (63 - page)
-            self.write_cmd(0x00 + (column & 0x0F))
-            self.write_cmd(0x10 + (column >> 4))
-            for num in range(0, 16):
-                self.write_data(self.buffer[page * 16 + num])
 
 # ---------------------- Main Program -----------------------
 
